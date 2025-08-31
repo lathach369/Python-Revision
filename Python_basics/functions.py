7b0e65c0f8c9366db156f915be933b999dffd123
latha <latha.ch.tech@gmail.com>
2025-08-31 22:12:45 +0530

    added functions concepts

---
+++ Python_basics/functions.py
@@ -0,0 +1,18 @@
+
+# Functions
+
+# def greet():
+#     print("Hi there")
+#     print("Hello world")
+
+
+# greet()
+
+
+# Arguments
+def greet(first_name, last_name):
+    print(f"hi {first_name} {last_name}")
+    print("Welcome aboard")
+
+
+greet("John", "Smith")
